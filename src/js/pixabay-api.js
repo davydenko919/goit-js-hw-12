cf3e755695626a783b78040c026e410c2338f2ce
davydenko919 <davydenko919@gmail.com>
2024-03-11 21:30:21 +0200

    ?

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,4 +1,4 @@
-import iziToast from 'izitoast';
+
 import axios from 'axios';
 
 const KEY = `42787384-4c627c93f7dff570902230658`;
@@ -6,25 +6,11 @@ const BASE_URI = `https://pixabay.com/api/`;
 
 
 export async function getImages(QUERY, page = 1) {
-  const LINK = `${BASE_URI}?key=${KEY}&q=${QUERY}&image_type=photo&per_page=15&page=${page}`;
-  // console.log(LINK)
   try {
-    const response = await axios.get(LINK);
-
-    if (response.data.hits.length > 15) {
-      if (page === 1) {
-        iziToast.error({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
-        document.querySelector('.load-btn').style.display = 'none';
-      } 
-     
-      document.querySelector('.load-btn').style.display = 'none';
-      return [];
-    }
-    return response.data;
+      const response = await axios.get(`${BASE_URI}?key=${KEY}&q=${QUERY}&image_type=photo&per_page=15&page=${page}`);
+      return response.data;
   } catch (error) {
-    console.error(`Error: ${error}`);
+      console.error('Error fetching data:', error);
+      throw error;
   }
 }
